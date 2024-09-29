59686bb49f4e8d6230296e68496b5d77ddf1f0a4
Peterpaul Klein Haneveld <pp.kleinhaneveld@gmail.com>
2024-09-29 20:40:10 +0200

    Avoid SQLite database is locked
    From https://stackoverflow.com/questions/57123453/how-to-use-diesel-with-sqlite-connections-and-avoid-database-is-locked-type-of

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,38 @@ pub mod models;
 pub mod modules;
 pub mod schema;
 
+use std::{env, time::Duration};
+
 use diesel::{
+    connection::SimpleConnection,
     prelude::*,
-    r2d2::{ConnectionManager, Pool},
+    r2d2::{self, ConnectionManager, CustomizeConnection, Pool},
 };
 use dotenvy::dotenv;
-use std::env;
 
-pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
+#[derive(Debug)]
+pub struct ConnectionOptions {
+    pub enable_wal: bool,
+    pub enable_foreign_keys: bool,
+    pub busy_timeout: Option<Duration>,
+}
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+impl CustomizeConnection<SqliteConnection, r2d2::Error> for ConnectionOptions {
+    fn on_acquire(&self, conn: &mut SqliteConnection) -> Result<(), r2d2::Error> {
+        (|| {
+            if self.enable_wal {
+                conn.batch_execute("PRAGMA journal_mode = WAL; PRAGMA synchronous = NORMAL;")?;
+            }
+            if self.enable_foreign_keys {
+                conn.batch_execute("PRAGMA foreign_keys = ON;")?;
+            }
+            if let Some(d) = self.busy_timeout {
+                conn.batch_execute(&format!("PRAGMA busy_timeout = {};", d.as_millis()))?;
+            }
+            Ok(())
+        })()
+        .map_err(r2d2::Error::QueryError)
+    }
 }
 
 pub fn get_connection_pool() -> Pool<ConnectionManager<SqliteConnection>> {
@@ -24,10 +43,14 @@ pub fn get_connection_pool() -> Pool<ConnectionManager<SqliteConnection>> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     let manager = ConnectionManager::<SqliteConnection>::new(database_url);
-    // Refer to the `r2d2` documentation for more methods to use
-    // when building a connection pool
+
     Pool::builder()
-        .test_on_check_out(true)
+        .max_size(16)
+        .connection_customizer(Box::new(ConnectionOptions {
+            enable_wal: true,
+            enable_foreign_keys: true,
+            busy_timeout: Some(Duration::from_secs(30)),
+        }))
         .build(manager)
         .expect("Could not build connection pool")
 }
