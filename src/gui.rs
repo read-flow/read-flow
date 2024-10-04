6bb2dc8337dd51c4d3a3d35e81cedadd2fb238a2
Peterpaul Klein Haneveld <pp.kleinhaneveld@gmail.com>
2024-10-04 22:25:43 +0200

    Display file_tags next to path

--- src/gui.rs
+++ src/gui.rs
@@ -10,6 +10,7 @@ use iced::{
     Color, Element, Task,
 };
 use iced_aw::{grid_row, Grid};
+use indexmap::IndexMap;
 
 use crate::{
     get_connection_pool,
@@ -49,7 +50,7 @@ struct Files {
     shorten_path: bool,
     ordering: OrderFilesBy,
     connection_pool: Option<ConnectionPool>,
-    files: Vec<File>,
+    files: Vec<(File, Vec<FileTag>)>,
     dialog: Option<Dialog>,
 }
 
@@ -83,7 +84,7 @@ enum Message {
     OpenDialog(Dialog),
     TagChanged(String),
     TagApplied(Result<FileTag, Error>),
-    FilesLoaded(Result<Vec<File>, Error>),
+    FilesLoaded(Result<Vec<(File, Vec<FileTag>)>, Error>),
     OrderBy(OrderFilesBy),
 }
 
@@ -189,19 +190,22 @@ impl Files {
             ])
             .column_spacing(10);
 
-        for file in self.files.iter() {
+        for (file, tags) in self.files.iter() {
             let path = if self.shorten_path {
                 file.path.clone().split('/').last().unwrap().to_string()
             } else {
                 file.path.clone()
             };
+
             grid = grid.push(grid_row![
                 row![button("tag").on_press(Message::OpenDialog(Dialog::file_tag(file.id))),],
                 text(file.id),
                 text(file.type_.clone()),
                 text(file.size),
                 text(format!("{}...", &file.sha256sum[..9])),
-                text(path),
+                row![text(path)]
+                    .extend(tags.iter().map(|tag| button(text(tag.tag.clone())).into()))
+                    .spacing(5),
             ]);
         }
 
@@ -252,8 +256,8 @@ where
 async fn query_files(
     connection_pool: ConnectionPool,
     order_by: OrderFilesBy,
-) -> Result<Vec<File>, Error> {
-    let files = match order_by {
+) -> Result<Vec<(File, Vec<FileTag>)>, Error> {
+    let files: Vec<File> = match order_by {
         OrderFilesBy::Id => files::dsl::files
             .order_by(files::columns::id)
             .load(&mut connection_pool.get().unwrap())?,
@@ -270,7 +274,21 @@ async fn query_files(
             .order_by(files::columns::sha256sum)
             .load(&mut connection_pool.get().unwrap())?,
     };
-    Ok(files)
+
+    let file_tags: Vec<FileTag> =
+        file_tags::dsl::file_tags.load(&mut connection_pool.get().unwrap())?;
+
+    let mut result: IndexMap<i32, (File, Vec<FileTag>)> = files
+        .into_iter()
+        .map(|file| (file.id, (file, Vec::new())))
+        .collect();
+
+    for tag in file_tags {
+        if let Some((_file, tags)) = result.get_mut(&tag.file_id) {
+            tags.push(tag);
+        }
+    }
+    Ok(result.into_values().collect())
 }
 
 async fn add_file_tag(
