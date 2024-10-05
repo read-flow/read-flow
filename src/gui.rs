2f1fb157b0291c7c8cc5f3893ffa3795ad3c25e3
Peterpaul Klein Haneveld <pp.kleinhaneveld@gmail.com>
2024-10-05 22:47:42 +0200

    Allow filtering files by tags

--- src/gui.rs
+++ src/gui.rs
@@ -2,12 +2,12 @@ use std::sync::Arc;
 
 use diesel::prelude::*;
 use iced::{
+    border,
     widget::{
-        button, center, column, container, mouse_area, opaque, row, scrollable,
-        scrollable::{Direction, Scrollbar},
-        stack, text, text_input,
+        button, center, column, container, mouse_area, opaque, row, scrollable, stack, text,
+        text_input,
     },
-    Color, Element, Task,
+    Color, Element, Task, Theme,
 };
 use iced_aw::{grid_row, Grid};
 use indexmap::IndexMap;
@@ -31,13 +31,16 @@ impl Dialog {
 
     fn to_element(&self) -> Element<Message> {
         match self {
-            Dialog::FileTag { tag, .. } => container(column![
-                row![text("Add tag")],
-                row![text_input("tag", &tag.clone().unwrap_or("".to_string()))
-                    .width(250)
-                    .on_input(Message::TagChanged)],
-                row![button("close").on_press(Message::CloseDialog)],
-            ])
+            Dialog::FileTag { tag, .. } => container(
+                column![
+                    row![text("Add tag")],
+                    row![text_input("tag", &tag.clone().unwrap_or("".to_string()))
+                        .width(250)
+                        .on_input(Message::TagChanged)],
+                    row![button("close").on_press(Message::CloseDialog)],
+                ]
+                .spacing(10),
+            )
             .style(container::rounded_box)
             .padding(10),
         }
@@ -52,6 +55,7 @@ struct Files {
     connection_pool: Option<ConnectionPool>,
     files: Vec<(File, Vec<FileTag>)>,
     dialog: Option<Dialog>,
+    selected_tags: Vec<String>,
 }
 
 #[derive(Debug, Clone, Copy, Default)]
@@ -86,10 +90,14 @@ enum Message {
     TagApplied(Result<FileTag, Error>),
     FilesLoaded(Result<Vec<(File, Vec<FileTag>)>, Error>),
     OrderBy(OrderFilesBy),
+    AddTagFilter(String),
+    RemoveTagFilter(String),
 }
 
 pub fn gui() -> iced::Result {
-    iced::application("ArchiveOrganizer - Files", Files::update, Files::view).run_with(Files::new)
+    iced::application("ArchiveOrganizer - Files", Files::update, Files::view)
+        .theme(|_| Theme::Nord)
+        .run_with(Files::new)
 }
 
 impl Files {
@@ -97,10 +105,11 @@ impl Files {
         let mut this: Self = Default::default();
         let ordering = this.ordering;
         let connection_pool = this.connection_pool();
+        let selected_tags = this.selected_tags.clone();
         (
             this,
             Task::batch([Task::perform(
-                query_files(connection_pool, ordering),
+                query_files_by_tags(connection_pool, ordering, selected_tags),
                 Message::FilesLoaded,
             )]),
         )
@@ -117,7 +126,11 @@ impl Files {
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::Update => Task::perform(
-                query_files(self.connection_pool(), self.ordering),
+                query_files_by_tags(
+                    self.connection_pool(),
+                    self.ordering,
+                    self.selected_tags.clone(),
+                ),
                 Message::FilesLoaded,
             ),
             Message::ToggleShortenPath => {
@@ -128,8 +141,14 @@ impl Files {
                 Some(Dialog::FileTag {
                     file_id,
                     tag: Some(tag),
-                }) => Task::perform(
-                    add_file_tag(self.connection_pool(), FileTag { file_id, tag }),
+                }) if !tag.trim().is_empty() => Task::perform(
+                    add_file_tag(
+                        self.connection_pool(),
+                        FileTag {
+                            file_id,
+                            tag: tag.trim().to_string(),
+                        },
+                    ),
                     Message::TagApplied,
                 ),
                 _ => Task::none(),
@@ -167,16 +186,20 @@ impl Files {
                 self.ordering = ordering;
                 Task::done(Message::Update)
             }
+            Message::AddTagFilter(tag) => {
+                self.selected_tags.push(tag);
+                Task::done(Message::Update)
+            }
+            Message::RemoveTagFilter(tag) => {
+                self.selected_tags.retain(|t| t != &tag);
+                Task::done(Message::Update)
+            }
         }
     }
 
     fn view(&self) -> Element<Message> {
         let action_bar = container(
-            row![
-                button("Update").on_press(Message::Update),
-                button("Toggle Short Path").on_press(Message::ToggleShortenPath)
-            ]
-            .spacing(10),
+            row![button("Toggle Short Path").on_press(Message::ToggleShortenPath)].spacing(10),
         );
 
         let mut grid = Grid::new()
@@ -186,7 +209,13 @@ impl Files {
                 button("type").on_press(Message::OrderBy(OrderFilesBy::Type)),
                 button("size").on_press(Message::OrderBy(OrderFilesBy::Size)),
                 button("fingerprint").on_press(Message::OrderBy(OrderFilesBy::Fingerprint)),
-                button("path").on_press(Message::OrderBy(OrderFilesBy::Path)),
+                row![button("path").on_press(Message::OrderBy(OrderFilesBy::Path))]
+                    .extend(self.selected_tags.iter().map(|t| {
+                        tag_button(t.clone())
+                            .on_press(Message::RemoveTagFilter(t.clone()))
+                            .into()
+                    }))
+                    .spacing(5),
             ])
             .column_spacing(10);
 
@@ -204,14 +233,22 @@ impl Files {
                 text(file.size),
                 text(format!("{}...", &file.sha256sum[..9])),
                 row![text(path)]
-                    .extend(tags.iter().map(|tag| button(text(tag.tag.clone())).into()))
+                    .extend(tags.iter().map(|tag| {
+                        if self.selected_tags.contains(&tag.tag) {
+                            tag_button(tag.tag.clone()).into()
+                        } else {
+                            tag_button(tag.tag.clone())
+                                .on_press(Message::AddTagFilter(tag.tag.clone()))
+                                .into()
+                        }
+                    }))
                     .spacing(5),
             ]);
         }
 
-        let table = scrollable(grid).direction(Direction::Both {
-            vertical: Scrollbar::new(),
-            horizontal: Scrollbar::new(),
+        let table = scrollable(grid).direction(scrollable::Direction::Both {
+            vertical: scrollable::Scrollbar::new(),
+            horizontal: scrollable::Scrollbar::new(),
         });
 
         // container(column![action_bar, table,]).into()
@@ -222,6 +259,13 @@ impl Files {
             None => content.into(),
         }
     }
+}
+
+fn tag_button(tag: String) -> button::Button<'static, Message> {
+    button(text(tag).size(10)).style(|theme, status| button::Style {
+        border: border::rounded(8),
+        ..button::primary(theme, status)
+    })
 }
 
 fn modal<'a, Message>(
@@ -253,30 +297,27 @@ where
     .into()
 }
 
-async fn query_files(
+async fn query_files_by_tags(
     connection_pool: ConnectionPool,
     order_by: OrderFilesBy,
+    tags: Vec<String>,
 ) -> Result<Vec<(File, Vec<FileTag>)>, Error> {
+    let mut connection = connection_pool.get().unwrap();
+
+    let files = files::table;
     let files: Vec<File> = match order_by {
-        OrderFilesBy::Id => files::dsl::files
-            .order_by(files::columns::id)
-            .load(&mut connection_pool.get().unwrap())?,
-        OrderFilesBy::Type => files::dsl::files
+        OrderFilesBy::Id => files.order_by(files::columns::id).load(&mut connection)?,
+        OrderFilesBy::Type => files
             .order_by(files::columns::type_)
-            .load(&mut connection_pool.get().unwrap())?,
-        OrderFilesBy::Path => files::dsl::files
-            .order_by(files::columns::path)
-            .load(&mut connection_pool.get().unwrap())?,
-        OrderFilesBy::Size => files::dsl::files
-            .order_by(files::columns::size)
-            .load(&mut connection_pool.get().unwrap())?,
-        OrderFilesBy::Fingerprint => files::dsl::files
+            .load(&mut connection)?,
+        OrderFilesBy::Path => files.order_by(files::columns::path).load(&mut connection)?,
+        OrderFilesBy::Size => files.order_by(files::columns::size).load(&mut connection)?,
+        OrderFilesBy::Fingerprint => files
             .order_by(files::columns::sha256sum)
-            .load(&mut connection_pool.get().unwrap())?,
+            .load(&mut connection)?,
     };
 
-    let file_tags: Vec<FileTag> =
-        file_tags::dsl::file_tags.load(&mut connection_pool.get().unwrap())?;
+    let file_tags: Vec<FileTag> = file_tags::table.load(&mut connection)?;
 
     let mut result: IndexMap<i32, (File, Vec<FileTag>)> = files
         .into_iter()
@@ -288,7 +329,14 @@ async fn query_files(
             tags.push(tag);
         }
     }
-    Ok(result.into_values().collect())
+
+    Ok(result
+        .into_values()
+        .filter(|(_file, file_tags)| {
+            let file_tags = file_tags.iter().map(|t| t.tag.clone()).collect::<Vec<_>>();
+            tags.iter().all(|tag| file_tags.contains(tag))
+        })
+        .collect())
 }
 
 async fn add_file_tag(
