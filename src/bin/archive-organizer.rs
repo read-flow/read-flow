e1ca9eec1651bf3a9bf25b56cde8e3dd241ab0fa
Peterpaul Klein Haneveld <pp.kleinhaneveld@gmail.com>
2025-03-10 18:30:55 +0100

    Remove client subcommand

--- src/bin/archive-organizer.rs
+++ src/bin/archive-organizer.rs
@@ -2,13 +2,11 @@ use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-use tokio::runtime::Runtime;
 use tracing_subscriber::{filter::EnvFilter, fmt, prelude::*};
-use url::Url;
 
 #[cfg(feature = "server")]
 use archive_organizer::server;
-use archive_organizer::{ApplicationModule, api::FileDataSource, client, gui::UiSettings};
+use archive_organizer::{ApplicationModule, gui::UiSettings};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -27,7 +25,6 @@ enum Commands {
     Gui(UiSettings),
     #[cfg(feature = "server")]
     Serve,
-    Client,
     ExtractScanDirectories,
 }
 
@@ -49,28 +46,6 @@ fn main() -> Result<()> {
         Commands::Scan { path } => ApplicationModule::instantiate()?.scan(path)?,
         #[cfg(feature = "server")]
         Commands::Serve => server::main(),
-        Commands::Client => {
-            // Create the runtime
-            let rt = Runtime::new().unwrap();
-
-            // Execute the future, blocking the current thread until completion
-            rt.block_on(async {
-                let client =
-                    client::FilesClient::new("http://localhost:8000/".parse::<Url>().unwrap())
-                        .unwrap();
-
-                let result = client.status().await;
-                tracing::info!("status result: {result:?}");
-
-                let result = client
-                    .download_file(4, &PathBuf::from("horse-power.pdf"))
-                    .await;
-                tracing::info!("download result: {result:?}");
-
-                let result = client.upload_file(&PathBuf::from("horse-power.pdf")).await;
-                tracing::info!("Uploaded as: {result:?}");
-            });
-        }
     };
 
     Ok(())
