c7cc82a5ac0fcecb48a9bc60712719e40557a4af
peterpaul <peterpaul@penguin>
2025-12-01 19:03:39 +0100

    Add cli arguments to cosmic application

--- cosmic/src/main.rs
+++ cosmic/src/main.rs
@@ -11,8 +11,20 @@ mod iter;
 mod page;
 mod state;
 
+use clap::Parser;
+
 use archive_organizer::ApplicationModule;
 use archive_organizer::settings;
+
+#[derive(Debug, clap::Parser)]
+pub struct Cli {
+    #[clap(long, default_value = "false")]
+    /// Enable private mode, which makes all `--private-tags` visible
+    private_mode: bool,
+    #[clap(long)]
+    /// Private tags and tagged files are hidden from the UI by default
+    private_tags: Vec<String>,
+}
 
 fn main() -> cosmic::iced::Result {
     // Initialize tracing
@@ -24,8 +36,17 @@ fn main() -> cosmic::iced::Result {
     // Enable localizations to be applied.
     i18n::init(&requested_languages);
 
+    // Parse commandline parameters.
+    let Cli {
+        private_mode,
+        private_tags,
+    } = Cli::parse();
+
     // Extract settings from the application's configuration.
-    let settings = settings::extract().expect("settings are present");
+    let mut settings = settings::extract().expect("settings are present");
+    // Merge commandline parameters with settings.
+    settings.ui.merge_in((private_mode, private_tags).into());
+
     let application_module = ApplicationModule::from_settings(settings);
 
     // Settings for configuring the application window and iced runtime.
