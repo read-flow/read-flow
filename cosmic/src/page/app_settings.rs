e2c2aef4efb7c1e472e17d7f283616590d96c58e
Peterpaul Klein Haneveld <pp.kleinhaneveld@gmail.com>
2026-03-09 21:23:01 +0100

    feat: Show radioboxes in a column layout

--- cosmic/src/page/app_settings.rs
+++ cosmic/src/page/app_settings.rs
@@ -7,9 +7,11 @@ use cosmic::Element;
 use cosmic::Task;
 use cosmic::cosmic_config;
 use cosmic::cosmic_config::CosmicConfigEntry;
+use cosmic::cosmic_theme;
 use cosmic::iced::Length;
 use cosmic::iced::alignment::Horizontal;
 use cosmic::iced::alignment::Vertical;
+use cosmic::theme;
 use cosmic::widget;
 
 use crate::ICON_SIZE;
@@ -44,28 +46,33 @@ impl Page for AppSettingsPage {
     type Message = AppSettingsMessage;
 
     fn view(&self) -> Element<'_, AppSettingsMessage> {
+        let cosmic_theme::Spacing { space_xs, .. } = theme::active().cosmic().spacing;
         let viewer_section = widget::settings::section()
             .title(fl!("settings-viewer-section"))
             .add(
                 widget::settings::item::builder(fl!("settings-epub-viewer"))
                     .description(fl!("settings-epub-viewer-description"))
                     .icon(widget::icon::from_name("application-epub+zip").size(ICON_SIZE))
-                    .control(widget::settings::item_row(vec![
-                        widget::radio(
-                            widget::text::body(fl!("settings-epub-viewer-native")),
-                            EpubViewerConfig::NativeEpub,
-                            Some(self.config.epub_viewer),
-                            AppSettingsMessage::SetEpubViewer,
-                        )
-                        .into(),
-                        widget::radio(
-                            widget::text::body(fl!("settings-epub-viewer-mupdf")),
-                            EpubViewerConfig::MuPdf,
-                            Some(self.config.epub_viewer),
-                            AppSettingsMessage::SetEpubViewer,
-                        )
-                        .into(),
-                    ])),
+                    .control(
+                        widget::Column::from_vec(vec![
+                            widget::radio(
+                                widget::text::body(fl!("settings-epub-viewer-native")),
+                                EpubViewerConfig::NativeEpub,
+                                Some(self.config.epub_viewer),
+                                AppSettingsMessage::SetEpubViewer,
+                            )
+                            .into(),
+                            widget::radio(
+                                widget::text::body(fl!("settings-epub-viewer-mupdf")),
+                                EpubViewerConfig::MuPdf,
+                                Some(self.config.epub_viewer),
+                                AppSettingsMessage::SetEpubViewer,
+                            )
+                            .into(),
+                        ])
+                        .spacing(space_xs)
+                        .align_x(Horizontal::Left),
+                    ),
             );
 
         layout(widget::settings::view_column(vec![viewer_section.into()]))
